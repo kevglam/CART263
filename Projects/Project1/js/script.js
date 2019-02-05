c0000688245bb542aa41fc39e46668afc396c1c4
Kevin G Lam <kevlam10@outlook.com>
2019-02-05 15:57:56 -0500

    Project01_ Added main buttons!

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -1,7 +1,48 @@
 "use strict";
 
+let $intro;
+
 $(document).ready(function(){
 
+  $intro = $('#intro');
 
-  
+  $('#start').on('click',function() {
+
+    if($intro.attr('src') === 'assets/images/intro.gif'){
+
+      $("#intro").attr("src", "assets/images/normalspd.gif");
+      }
+
+      else {
+        $("#intro").attr("src", "assets/images/normalspd.gif");
+      }
+
+
+    });
+
+    $('#faster').on('click',function() {
+
+      if($intro.attr('src') === 'assets/images/normalspd.gif'){
+
+        $("#intro").attr("src", "assets/images/fastspd.gif");
+        }
+
+        else if($intro.attr('src') === 'assets/images/fastspd.gif') {
+          $("#intro").attr("src", "assets/images/firespd.gif");
+        }
+
+        else {
+          $("#intro").attr("src", "assets/images/firespd.gif");
+        }
+
+
+      });
+
+      $('#break').on('click',function() {
+
+        $("#intro").attr("src", "assets/images/breaktime.jpg");
+
+
+        });
+
 });
